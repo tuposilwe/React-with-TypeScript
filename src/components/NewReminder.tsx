d602748b19fd44b689089b0c37e44fb02d992609
tuposilwe <rudigermkondya@gmail.com>
2024-07-20 18:05:17 +0300

    Handling Form Submission

--- src/components/NewReminder.tsx
+++ src/components/NewReminder.tsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
 
-function NewReminder(): JSX.Element {
+interface NewReminderProps {
+    onAddReminder: (title: string) => void;
+}
+
+function NewReminder({onAddReminder}: NewReminderProps): JSX.Element {
     const [title,setTitle] = useState('');
+
+   const submitForm = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(!title) return;
+    onAddReminder(title);
+    setTitle('');
+    
+   }
+
     return (
-        <form>
+        <form onSubmit={submitForm}>
             <label htmlFor="title"></label>
             <input value={title} onChange={e => setTitle(e.target.value)} type="title" className="form-control" />
             <button type='submit' className="btn btn-primary my-3 rounded-pill">Add Reminder</button>
