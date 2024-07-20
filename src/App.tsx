2de748875e665cc7a2c458bfc2744f781c6aad40
tuposilwe <rudigermkondya@gmail.com>
2024-07-20 16:57:24 +0300

    using the state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/reminder';
 
-const reminders: Reminder[] = [
-  {id: 1,title: 'Reminder 1'}
-]
- 
+
 function App() {
+  const [reminders,setReminders] = useState<Reminder[]>([
+    {id: 1,title: 'Reminder 1'}
+  ]);
+
   return (
     <div className="App">
      <ReminderList items={reminders}/>
